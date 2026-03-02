eff748c65df290a4c55c460946ebcdedc71a9fbc
hringbauer <hringbauer@gmx.at>
2026-03-02 23:19:12 +0100

    Derived Table

--- python/pulldown.py
+++ python/pulldown.py
@@ -103,7 +103,10 @@ def call_y_bam(path_bam="", df=[],
                path_bed = "/mnt/archgen/users/hringbauer/git/y_chrom/data/isogg_snps.bed",
                path_temp="/mnt/archgen/users/hringbauer/git/y_chrom/temp/temp.tsv",
                snip5=0, snip3=0):
-    """Creates the Call Table from a .bam file"""
+    """Return the Call Table from a .bam file"""
+    ### Sanity Checks whether Input is there
+    assert(os.path.exists(path_bam))
+    assert(os.path.exists(path_bed))
     
     ### Create the Pulldown
     pulldown_bamtable(path_bam = path_bam,
@@ -111,6 +114,7 @@ def call_y_bam(path_bam="", df=[],
                       snip5=snip5, snip3=snip3,
                       o_file = path_temp)
 
+    ### Load and formt pulldown
     df1 = pd.read_csv(path_temp, sep="\t", header=None)
     df1.columns = ["chrom", "pos", "A", "C", "G", "T"]
     idx = df1["chrom"]=="chrY"
@@ -165,3 +169,129 @@ def calc_error_rate(df_ch, haploclade="G"):
     ref = np.sum(dft["ref#"])
     print((ref,der))
     print(f"Error rate: {der/(ref+der)*100:.4g}%")
+
+
+################################
+### Functions for OY database
+
+### Prepare Dictionary of Levels
+
+def create_parent_dct(path_parents="/mnt/archgen/users/eric_garcia/OYdb/OYchpar.csv"):
+    """"Create Dictionary of Parent Nodes"""
+    chpar = {}
+    
+    # Create a dictionary to store all child-parent relations.
+    with open(path_parents, "r") as f:
+        for line in f:
+            items = line.strip().split(sep=",")
+            if not items:
+                continue
+                    
+            child = items[0]
+            parent = items[1]
+                
+            chpar[child] = parent
+    
+    return chpar
+
+
+# Create a function to get the total number of SNPs per branch and divide between derived and ancestral.
+def div_anc_der(df, chpar=[], df_exclude=[]):
+    """
+    Calculate total, ancestral, derived, and uncovered SNPs per branch.
+    df_exclude: Dataframe of SNPs to filter
+    """
+    if len(df_exclude)>0:
+        df = exclude_snps(df, df_exclude)
+    
+    if len(chpar)==0:
+        chpar = create_parent_dct()
+    
+    # Create columns for ANC and DER SNPs.
+    df = df.copy()
+    df["ancestral"] = df["alt#"] < df["ref#"]
+    df["derived"]   = df["alt#"] > df["ref#"]
+
+    # Divide the df by the different haplogroups.
+    grouped = df.groupby("Y-haplogroup")
+
+    # Generate a new df with columns for the Branch, the Level and the total for SNPs.
+    new_df = grouped.agg(
+        Level=("Level", "first"),
+        Total_SNPs=("Y-haplogroup", "size"),
+        Ancestral=("ancestral", "sum"),
+        Derived=("derived", "sum"),
+    ).reset_index()
+
+    # Compute uncovered as the difference of the total with ancestral and derived.
+    new_df["Uncovered"] = (
+        new_df["Total_SNPs"]
+        - new_df["Ancestral"]
+        - new_df["Derived"]
+    )
+
+    # Rename column called Y-haplogroup
+    new_df = new_df.rename(columns={"Y-haplogroup": "Branch"})
+    new_df = new_df.sort_values(by="Level")
+
+    # Use previously defined function to look for all ANC and DER SNPs in parental branches. First, transform into a dictionary to speed up processing.
+    anc_lookup = new_df.set_index("Branch")["Ancestral"].to_dict()
+    der_lookup = new_df.set_index("Branch")["Derived"].to_dict()
+    
+    anc_par = []
+    der_par = []
+
+    # For each branch, find the total number of ancestral and derived SNPs in parent using ancder_par() function.
+    for branch in new_df["Branch"]:
+        anc_par.append(ancder_par(branch, anc_lookup, chpar=chpar))
+        der_par.append(ancder_par(branch, der_lookup, chpar=chpar))
+
+    # Insert new columns.
+    new_df["#ANC in par."] = anc_par
+    new_df["#DER in par."] = der_par
+    
+    # Save a csv file
+    #new_df.to_csv(f"data/branches_statitstics_{sample}.csv")
+
+    return new_df
+
+def ancder_par(string, par_dict, chpar):
+    """ Find the total number of ancestral and derived SNPs for all parental branches"""
+
+    if string not in chpar:
+        return 0
+
+    else:
+        parent = chpar.get(string)
+        return ancder_par(parent, par_dict, chpar) + par_dict.get(parent, 0)
+
+def get_mismatch_snps(string, chpar, df_ch):
+    """Get all SNPs that are mismatches of Haplogroup String"""
+    dfs_mm = [] # List of mismatching SNP dfs
+    
+    while True:        
+        ### Find all mismatches
+        dft = df_ch[df_ch["Y-haplogroup"]==string] # All SNPs in Node
+        dfd =dft[dft["ref#"]>dft["alt#"]]
+        dfs_mm.append(dfd)  	
+
+        if string not in chpar: # If Root exit
+            dfs_mm = pd.concat(dfs_mm)
+            return dfs_mm
+        
+        else:
+            string = chpar[string] # Get Parent Node
+
+
+def exclude_snps(df_ch, df_ex, verbose=False, col="Subgroup Name"):
+    """Filter SNPs from df_ch in df_ex"""
+
+    idx= df_ch[col].isin(df_ex[col])
+    df_ch2 = df_ch[~idx].copy()
+    if verbose:
+        print(f"Filtered to {len(df_ch2)}/{len(df_ch)}")
+    return df_ch2
+
+
+
+    
